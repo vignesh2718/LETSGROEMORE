9d9ac85c6c0e080736c105d3080927f596d753d4
VIGNESH N <65857628+vignesh2718@users.noreply.github.com>
2022-01-19 15:58:30 +0530

    Add files via upload

---
+++ TASK-2/src/components/Footer.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Card.css";
+function Footer() {
+  return (
+    <>
+      <div className="footer">
+        <div class="contain">
+          <div class="col">
+            <ul>
+              <li>
+                Contact: <a href="mailto:flyhigh@gmail.com">contact@TravelZz.com</a>
+              </li>
+              <li>
+                Telphone: <a href="tel: +919074392096">+91 9074392096</a>
+              </li>
+            </ul>
+          </div>
+          <div class="clearfix"></div>
+        </div>
+      </div>
+    </>
+  );
+}
+export default Footer;
